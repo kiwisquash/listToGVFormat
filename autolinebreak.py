9520e455928dbae3ab3936b2f956ac8894e54454
Ki Song <kiwisquash@gmail.com>
2018-04-23 03:58:58 -0700

    Not quite working...

--- autolinebreak.py
+++ autolinebreak.py
@@ -1,4 +1,18 @@
 inputString = "Hello, World! My name is Ki."
-width = 10
 
+# Find the blank that appears just before the width.
 
+def findBlankIndex(inputString,width):
+    index = inputString.find(" ")
+    if (index < width):
+        while (inputString.find(" ") > 0):
+            if (index >= width):
+                inputString = inputString[index+1:]
+            index += inputString.find(" ")+1
+    return index
+
+for width in range(len(inputString)):
+    blankIndex = findBlankIndex(inputString, width)
+    print(blankIndex)
+    print(inputString[blankIndex])
+    print(inputString[:blankIndex]+"\n"+inputString[blankIndex+1:])
